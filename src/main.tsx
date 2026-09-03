3265a864a8c4f00f592707a8c5bafc0f29e19e63
backtonemesis-cpu <backtonemesis@gmail.com>
2026-09-03 22:06:16 +0100

    Gate production UI behind verified Firebase sign-in

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './firebase';
 import App from './App';
+import { AuthGate } from './components/AuthGate';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <AuthGate>
+      <App />
+    </AuthGate>
   </React.StrictMode>
 );
