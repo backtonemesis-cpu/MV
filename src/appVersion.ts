ddfc4b237bae461b766171de3ca7f9b03af0945f
backtonemesis-cpu <backtonemesis@gmail.com>
2026-09-04 16:57:52 +0100

    Add explicit MV application version

---
+++ src/appVersion.ts
@@ -0,0 +1,8 @@
+/**
+ * Human-readable MV application release number.
+ *
+ * This is intentionally separate from HouseholdData.version, which tracks
+ * local financial-data mutations. Increment this value when a new MV app
+ * release is deployed.
+ */
+export const APP_VERSION = '205';
