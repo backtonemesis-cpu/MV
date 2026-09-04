9d1823b27e1d6143871ac4c71e164f0a0a9bc6b4
backtonemesis-cpu <backtonemesis@gmail.com>
2026-09-04 03:39:19 +0100

    Run MV directly without Firebase auth gate

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './firebase';
 import App from './App';
-import { AuthGate } from './components/AuthGate';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthGate>
-      <App />
-    </AuthGate>
+    <App />
   </React.StrictMode>
 );
