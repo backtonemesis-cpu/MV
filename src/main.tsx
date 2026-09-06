d457bbf098484e72270c15d1cf8689ef24d515ca
backtonemesis-cpu <backtonemesis@gmail.com>
2026-09-06 17:40:17 +0100

    Stage 4: load accessibility overrides last

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import './globalDesignSystem.css';
+import './accessibility.css';
 import { applyThemePreferences, readStoredUserPreferences } from './themeEngine';
 import { installModalAccessibility } from './utils/modalAccessibility';
 
